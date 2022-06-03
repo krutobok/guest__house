06aa22b290754623414dec45ed92cecadd6fce72
Alexander <sasha020604336618@gmail.com>
2022-06-03 11:40:56 +0300

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -32,9 +32,36 @@ function rate() {
 function slider(sliderContent) {
     const slider = document.querySelector('.' + sliderContent)
     const items = document.querySelectorAll('.slider__item')
-    const width = parseInt(window.getComputedStyle(items[0]).getPropertyValue('flex-basis').substr(0,3))
+    const width = parseInt(window.getComputedStyle(items[0]).getPropertyValue('flex-basis').substring(0,3))
+    const margin = parseInt(window.getComputedStyle(items[0]).getPropertyValue('margin-right').substring(0,2))
+    let height = parseInt(window.getComputedStyle(slider).getPropertyValue('height').substring(0,3));
+        + parseInt(window.getComputedStyle(slider).getPropertyValue('padding-bottom').substring(0,2))
+    const parent = slider.closest('.slider__content-inner')
+    const btnRight = document.querySelector('.slider__btn-right')
+//     items.forEach(item => {
+//         const itemHeight = parseInt(window.getComputedStyle(item).getPropertyValue('height').substr(0,3))
+//             + parseInt(window.getComputedStyle(item).getPropertyValue('padding-top').substr(0,2))
+//             + parseInt(window.getComputedStyle(item).getPropertyValue('padding-bottom').substr(0,2))
+//         console.log(itemHeight)
+//         if (height < itemHeight){
+//             height = itemHeight
+//         }
+//     })
+    parent.style.width = (items.length * width + (items.length-1)*margin) + 'px'
+    parent.style.height = height + 'px'
+    console.log(btnRight)
+    let sliderPos = window.getComputedStyle(slider).getPropertyValue('left')
+    sliderPos = parseInt(sliderPos.substring(0, sliderPos.length - 2))
+    console.log( sliderPos)
+    btnRight.addEventListener('click', ()=> {
+        sliderPos = sliderPos - width - margin
+        slider.style.left = sliderPos + 'px'
+
+    })
 }
 
-slider('slider__content')
-
 rate()
+
+document.addEventListener('DOMContentLoaded', () =>{
+    slider('slider__content')
+})
